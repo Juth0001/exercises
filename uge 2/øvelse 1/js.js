555ec7047e178e5559c8b4b5e04c0686a87f3380
Julie <juth0001@stud.kea.dk>
2025-09-01 14:02:12 +0200

    uge 2 + opgave, og mappe uge 2 oprettet m. dokumenter

---
+++ uge 2/øvelse 1/js.js
@@ -0,0 +1,57 @@
+"use strict";
+
+/*let letters = ["a", "b", "c", "d", "e", "f", "g", "h"];
+console.log(letters[4]);
+let newarr = letters;
+newarr[4] = "*";
+console.log(letters[4]);
+
+
+const people = ["harry", "hermione", "ron"];
+console.log("people", people);
+let result = people.shift();
+*/
+
+
+/*
+function conGreet(firstName) {
+    console.log(`hello ${firstName}`);
+}
+
+setTimeout(conGreet, 1000, "Ron");
+*/
+
+
+
+/*
+const person3 = {
+    firstName: "Harry",
+    lastName: "Potter",
+    hired: false,
+};
+const person4 = {
+    firstName: "Fred",
+    lastName: "Wesley",
+    hired: false,
+};
+console.log(person4);
+hire(person4);
+console.log(person4);
+
+function hire(person) {
+    person = hired = true;
+}
+function fire(person) {
+    person.hired = false;
+}
+
+function fireOrHire(action, person) {
+    action(person);
+}
+console.log(person3);
+fireOrHire(hire, person3);
+console.log(person3);
+*/
+
+
+
