2ddce12f7cd7b423804f63147bbf3af4458aa7cf
Julie <juth0001@stud.kea.dk>
2025-09-05 09:56:35 +0200

    name parts opgave

---
+++ uge 2/string methods/øvelse 2/index.js
@@ -0,0 +1,12 @@
+const name = "Peter Heronimous Lind";
+const firstName = name.substring(0, 5);
+const middleName = name.substring(6, 16);
+const lastNames = name.substring(17, 21);
+const len = name.length;
+console.log(`${name} is ${len} caracters long`);
+
+
+
+console.log(`${firstName}`);
+console.log(`${middleName}`);
+console.log(`${lastNames}`);
